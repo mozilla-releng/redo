fd69430eb65450469154c3b7a881544340b2058b
Ben Hearsum <bhearsum@mozilla.com>
2014-04-27 14:26:12 -0700

    Add command line retry.

---
+++ cmd.py
@@ -0,0 +1,42 @@
+import logging
+from subprocess import check_call, CalledProcessError
+import sys
+
+from retry import retrying
+
+log = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument("-a", "--attempts", type=int, default=5,
+        help="How many times to retry.")
+    parser.add_argument("-s", "--sleeptime", type=int, default=60,
+        help="How long to sleep between attempts. Sleeptime doubles after each attempt.")
+    parser.add_argument("-m", "--max-sleeptime", type=int, default=5*60,
+        help="Maximum length of time to sleep between attempts (limits backoff length).")
+    parser.add_argument("-v", "--verbose", type=bool, action="store_true", default=False)
+    parser.add_argument("cmd",
+        help="Command to run. Eg: wget http://blah")
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.INFO)
+        logging.getLogger("retry").setLevel(logging.INFO)
+    else:
+        logging.basicConfig(level=logging.ERROR)
+        logging.getLogger("retry").setLevel(logging.ERROR)
+
+    try:
+        with retrying(check_call, attempts=args.attempts, sleeptime=args.sleeptime,
+                      max_sleeptime=args.max_sleeptime,
+                      retry_exceptions=(CalledProcessError,)):
+            check_call(args.cmd)
+    except KeyboardInterrupt:
+        sys.exit(-1)
+    except Exception, e:
+        log.error("Unable to run command after %d attempts" % args.attempts)
+        rc = getattr(e, "returncode", -2)
+        sys.exit(rc)
