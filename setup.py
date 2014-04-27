8e7a2da3024849e4cd2ba27dfad1fee3bfac9f99
Ben Hearsum <bhearsum@mozilla.com>
2014-04-27 13:56:26 -0700

    Add setup file.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="retry",
+    version=__version__,
+    description="Utilities to retry Python callables.",
+    author="Mozilla Release Engineering",
+    author_email="release+pypi@mozilla.com",
+    packages=["retry"],
+)
