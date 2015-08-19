6408b592fae233eb19a06e7183249685485b9a9e
Ben Hearsum <bhearsum@mozilla.com>
2015-08-19 10:22:44 -0400

    Fix up taskclusterrc changes, and switch to tox.

--- runtests.sh
+++
@@ -1,7 +0,0 @@
-rm -rf test
-virtualenv test
-source test/bin/activate
-pip install pep8 pyflakes nose
-pep8 --config=./pep8rc .
-pyflakes .
-nosetests --with-doctest
--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 
 setup(
     name="redo",
