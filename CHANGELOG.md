373651d74873475406b10b3bdf2d06dbacf82486
Tom Prince <mozilla@hocat.ca>
2018-11-26 13:06:25 -0700

    2.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 2.0.2
+
+* Fix support in `redo.cmd` for commands with arguments.
+
 # 2.0.1
 
 * Lazily format log messages, so we don't incur costly str() calculations in some cases. Contributed by @samueldg
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(
     name="redo",
-    version="2.0.1",
+    version="2.0.2",
     description="Utilities to retry Python callables.",
     author="Ben Hearsum",
     author_email="ben@hearsum.ca",
