16cdfb4eef7a0eb6543ba3e5362dae4136134435
Ben Hearsum <bhearsum@mozilla.com>
2020-09-14 15:48:26 -0400

    Add LICENSE and version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.rst") as f:
 
 setup(
     name="redo",
-    version="2.0.3",
+    version="2.0.4",
     description="Utilities to retry Python callables.",
     long_description=long_description,
     author="Ben Hearsum",
